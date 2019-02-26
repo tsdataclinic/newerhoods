29a52612ee9399811dfb6bb80ac0448a5a404564
Kaushik Mohan <km4326@nyu.edu>
2019-02-26 12:53:40 -0500

    adding cleaned data files to git repo. changed directory structure of data and corresponding paths in server

--- neighborhood-reclassification/server.R
+++ neighborhood-reclassification/server.R
@@ -44,11 +44,11 @@ add_legend <- function(plot_type){
 }
 
 ### Loading data
-load("../data/sales_features_2017.RData")
-load("../data/crime_rates.RData")
-load("../data/nyc311_rates.RData")
-load("../data/census_tracts.RData")
-census_pop <- read_xlsx("../data/2010census_population.xlsx",skip=7,
+load("../data/cleaned/sales_features_2017.RData")
+load("../data/cleaned/crime_rates.RData")
+load("../data/cleaned/nyc311_rates.RData")
+load("../data/cleaned/census_tracts.RData")
+census_pop <- read_xlsx("../cleaned/data/2010census_population.xlsx",skip=7,
                         col_names=c("borough","county_code","borough_code",
                                     "2010_tract","pop_2000","pop_2010",
                                     "change","pct_change","acres","pop_per_acre"))
@@ -175,7 +175,7 @@ function(input, output) {
   ## To Do: Option to not have any baseline map
   ### Baseline Map
   baseline_map <- eventReactive(input$baseline,{
-    load(file=paste0("../data/",input$baseline,".RData"))
+    load(file=paste0("../data/cleaned/",input$baseline,".RData"))
     get(input$baseline)
   })
   
